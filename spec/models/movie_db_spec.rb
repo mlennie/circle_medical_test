4f1271192dd9b30a62b659e9286919a16f49b3e8
Monty Lennie <montylennie@gmail.com>
2022-04-11 23:14:32 +0000

    get first unit test working

--- spec/models/movie_db_spec.rb
+++ spec/models/movie_db_spec.rb
@@ -2,15 +2,17 @@ RSpec.describe 'MovieDb' do
   context 'with correct params' do
     it 'return correctly formatted results' do
       results = MovieDb.search_by_query_for_all_pages("batman")
-      expect { results.length }.to equal(20)
+      expect(results.length).to equal(20)
       expected_first_results = {
         id: 414906,
         url: "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
         title: "The Batman",
         overview: "In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler."
       }
-      binding.pry
-      expect {results[0][:id]}.to equal(414906)
+      expect(results[0][:id]).to equal(414906)
+      expect(results[0][:url]).to eq("https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg")
+      expect(results[0][:title]).to eq("The Batman")
+      expect(results[0][:overview]).to eq("In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.")
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,8 +20,8 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('app', 'classes', 'movie_db.rb')].sort.each { |f| require f }
-require File.join(File.dirname(FILE), ‘…/…/app/classes/movie_db.rb’)
+#Dir[Rails.root.join('app', 'classes', 'movie_db.rb')].sort.each { |f| require f }
+#require File.join(File.dirname(FILE), ‘…/…/app/classes/movie_db.rb’)
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
